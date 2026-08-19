7c5f464e8c1bfa0d6afd27400fdf495280ef1e09
Abdelhamid <hamid.man14@gmail.com>
2026-08-20 00:46:42 +0200

    init

---
+++ src/main/java/com/recherche/offre/recherche_offre_api/RechercheOffreApiApplication.java
@@ -0,0 +1,13 @@
+package com.recherche.offre.recherche_offre_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechercheOffreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RechercheOffreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recherche/offre/recherche_offre_api/RechercheOffreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recherche.offre.recherche_offre_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechercheOffreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
